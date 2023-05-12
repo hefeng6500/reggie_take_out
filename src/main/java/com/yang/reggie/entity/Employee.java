9073a8e071a9ee10031bfdce725a5a07a3127ca7
hefeng6500 <2443992009@qq.com>
2023-05-12 20:19:38 +0800

    fix: error

--- src/main/java/com/yang/reggie/entity/Employee.java
+++ src/main/java/com/yang/reggie/entity/Employee.java
@@ -2,42 +2,66 @@ package com.yang.reggie.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@TableName("employee")
 @Data
 public class Employee implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
+    /**
+     * 主键
+     */
     private Long id;
-
-    private String username;
-
+    /**
+     * 姓名
+     */
     private String name;
-
-    @JsonIgnore
-    @TableField(value = "password", select = false)
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
     private String password;
-
+    /**
+     * 手机号
+     */
     private String phone;
-
+    /**
+     * 性别
+     */
     private String sex;
-
+    /**
+     * 身份证号
+     */
     private String idNumber;
-
+    /**
+     * 状态 0:禁用，1:正常
+     */
     private Integer status;
-
-    private LocalDateTime createTime;
-
-    private LocalDateTime updateTime;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Long createUser;
-
+    /**
+     * 创建时间
+     */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime createTime;
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long createUser;
+    /**
+     * 修改人
+     */
+    @TableField(fill = FieldFill.UPDATE)
     private Long updateUser;
-
 }
