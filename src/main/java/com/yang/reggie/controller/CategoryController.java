2cf7e48cd07482607a0483cbadd4a8bbe0486314
hefeng6500 <2443992009@qq.com>
2023-05-13 18:47:52 +0800

    feat: 添加根据分类类型查询菜品分类列表

--- src/main/java/com/yang/reggie/controller/CategoryController.java
+++ src/main/java/com/yang/reggie/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Slf4j
@@ -102,5 +103,23 @@ public class CategoryController {
     log.info("删除菜品分类信息：" + id);
 
     return R.success("删除成功！");
+  }
+
+  /**
+   * 根据条件查询分类数据
+   * @param category
+   * @return
+   */
+  @GetMapping("/list")
+  public R<List<Category>> list(Category category){
+    //条件构造器
+    LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+    //添加条件
+    queryWrapper.eq(category.getType() != null,Category::getType,category.getType());
+    //添加排序条件
+    queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+
+    List<Category> list = categoryService.list(queryWrapper);
+    return R.success(list);
   }
 }
