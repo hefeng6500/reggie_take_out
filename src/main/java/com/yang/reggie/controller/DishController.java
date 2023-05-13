ec1f574d06a62f11dd66248d1d63a8bd0f33f967
hefeng6500 <2443992009@qq.com>
2023-05-13 21:21:53 +0800

    feat: 套餐管理

--- src/main/java/com/yang/reggie/controller/DishController.java
+++ src/main/java/com/yang/reggie/controller/DishController.java
@@ -7,10 +7,12 @@ import com.yang.reggie.common.R;
 import com.yang.reggie.dto.DishDto;
 import com.yang.reggie.entity.Category;
 import com.yang.reggie.entity.Dish;
+import com.yang.reggie.entity.DishFlavor;
 import com.yang.reggie.service.CategoryService;
 import com.yang.reggie.service.DishFlavorService;
 import com.yang.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -36,11 +38,12 @@ public class DishController {
 
   /**
    * 新增菜品
+   *
    * @param dishDto
    * @return
    */
   @PostMapping
-  public R<String> save(@RequestBody DishDto dishDto){
+  public R<String> save(@RequestBody DishDto dishDto) {
     log.info(dishDto.toString());
 
     dishService.saveWithFlavor(dishDto);
@@ -50,43 +53,44 @@ public class DishController {
 
   /**
    * 菜品信息分页查询
+   *
    * @param page
    * @param pageSize
    * @param name
    * @return
    */
   @GetMapping("/page")
-  public R<Page> page(int page, int pageSize, String name){
+  public R<Page> page(int page, int pageSize, String name) {
 
-    //构造分页构造器对象
-    Page<Dish> pageInfo = new Page<>(page,pageSize);
+    // 构造分页构造器对象
+    Page<Dish> pageInfo = new Page<>(page, pageSize);
     Page<DishDto> dishDtoPage = new Page<>();
 
-    //条件构造器
+    // 条件构造器
     LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
-    //添加过滤条件
-    queryWrapper.like(name != null,Dish::getName,name);
-    //添加排序条件
+    // 添加过滤条件
+    queryWrapper.like(name != null, Dish::getName, name);
+    // 添加排序条件
     queryWrapper.orderByDesc(Dish::getUpdateTime);
 
-    //执行分页查询
-    dishService.page(pageInfo,queryWrapper);
+    // 执行分页查询
+    dishService.page(pageInfo, queryWrapper);
 
-    //对象拷贝
-    BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+    // 对象拷贝
+    BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
 
     List<Dish> records = pageInfo.getRecords();
 
     List<DishDto> list = records.stream().map((item) -> {
       DishDto dishDto = new DishDto();
 
-      BeanUtils.copyProperties(item,dishDto);
+      BeanUtils.copyProperties(item, dishDto);
 
-      Long categoryId = item.getCategoryId();//分类id
-      //根据id查询分类对象
+      Long categoryId = item.getCategoryId();// 分类id
+      // 根据id查询分类对象
       Category category = categoryService.getById(categoryId);
 
-      if(category != null){
+      if (category != null) {
         String categoryName = category.getName();
         dishDto.setCategoryName(categoryName);
       }
@@ -100,11 +104,12 @@ public class DishController {
 
   /**
    * 根据id查询菜品信息和对应的口味信息
+   *
    * @param id
    * @return
    */
   @GetMapping("/{id}")
-  public R<DishDto> get(@PathVariable Long id){
+  public R<DishDto> get(@PathVariable Long id) {
 
     DishDto dishDto = dishService.getByIdWithFlavor(id);
 
@@ -113,16 +118,70 @@ public class DishController {
 
   /**
    * 修改菜品
+   *
    * @param dishDto
    * @return
    */
   @PutMapping
-  public R<String> update(@RequestBody DishDto dishDto){
+  public R<String> update(@RequestBody DishDto dishDto) {
     log.info(dishDto.toString());
 
     dishService.updateWithFlavor(dishDto);
 
     return R.success("新增菜品成功");
+  }
+
+
+  /**
+   * 根据条件查询对应的菜品数据
+   * @param dish
+   * @return
+   */
+    /*@GetMapping("/list")
+    public R<List<Dish>> list(Dish dish){
+        //构造查询条件
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(dish.getCategoryId() != null ,Dish::getCategoryId,dish.getCategoryId());
+        //添加条件，查询状态为1（起售状态）的菜品
+        queryWrapper.eq(Dish::getStatus,1);
+
+        //添加排序条件
+        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
+
+        List<Dish> list = dishService.list(queryWrapper);
+
+        return R.success(list);
+    }*/
+  @GetMapping("/list")
+  public R<List<DishDto>> list(Dish dish) {
+    log.info("dish:{}", dish);
+    // 条件构造器
+    LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+    queryWrapper.like(StringUtils.isNotEmpty(dish.getName()), Dish::getName, dish.getName());
+    queryWrapper.eq(null != dish.getCategoryId(), Dish::getCategoryId, dish.getCategoryId());
+    // 添加条件，查询状态为1（起售状态）的菜品
+    queryWrapper.eq(Dish::getStatus, 1);
+    queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+
+    // 查询对应菜品的分类和口味信息
+    List<Dish> dishs = dishService.list(queryWrapper);
+
+    List<DishDto> dishDtos = dishs.stream().map(item -> {
+      DishDto dishDto = new DishDto();
+      BeanUtils.copyProperties(item, dishDto);
+      Category category = categoryService.getById(item.getCategoryId());
+      if (category != null) {
+        dishDto.setCategoryName(category.getName());
+      }
+      LambdaQueryWrapper<DishFlavor> wrapper = new LambdaQueryWrapper<>();
+      wrapper.eq(DishFlavor::getDishId, item.getId());
+
+      dishDto.setFlavors(dishFlavorService.list(wrapper));
+      return dishDto;
+    }).collect(Collectors.toList());
+
+    return R.success(dishDtos);
   }
 }
 
---
+++ src/main/java/com/yang/reggie/service/SetmealDishService.java
@@ -0,0 +1,7 @@
+package com.yang.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yang.reggie.entity.SetmealDish;
+
+public interface SetmealDishService extends IService<SetmealDish> {
+}
