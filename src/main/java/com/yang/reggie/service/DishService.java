6b2abf55ce58bb83e84102aa316e929006ea9a37
hefeng6500 <2443992009@qq.com>
2023-05-13 14:28:48 +0800

    feat: 创建 Dish 和 Setmeal 基础类

---
+++ src/main/java/com/yang/reggie/service/DishService.java
@@ -0,0 +1,7 @@
+package com.yang.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yang.reggie.entity.Dish;
+
+public interface DishService extends IService<Dish> {
+}
---
+++ src/main/java/com/yang/reggie/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,14 @@
+package com.yang.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yang.reggie.entity.Setmeal;
+import com.yang.reggie.mapper.SetmealMapper;
+import com.yang.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+}
