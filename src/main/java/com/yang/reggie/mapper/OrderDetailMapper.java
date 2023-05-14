512682763988b244df6a8b5b901711f93bc7eb92
hefeng6500 <2443992009@qq.com>
2023-05-14 20:43:25 +0800

    feat: 添加下单功能

---
+++ src/main/java/com/yang/reggie/mapper/OrderDetailMapper.java
@@ -0,0 +1,10 @@
+package com.yang.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yang.reggie.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
---
+++ src/main/java/com/yang/reggie/service/OrderDetailService.java
@@ -0,0 +1,9 @@
+package com.yang.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yang.reggie.entity.OrderDetail;
+
+public interface OrderDetailService extends IService<OrderDetail> {
+
+}
+
