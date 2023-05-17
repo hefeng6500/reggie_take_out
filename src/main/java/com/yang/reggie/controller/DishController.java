f7b476c25a09a2173d4fe9027aac19f9830ac481
hefeng6500 <2443992009@qq.com>
2023-05-17 22:25:14 +0800

    feat: 添加起售停售状态切换

--- src/main/java/com/yang/reggie/controller/DishController.java
+++ src/main/java/com/yang/reggie/controller/DishController.java
@@ -11,11 +11,14 @@ import com.yang.reggie.service.CategoryService;
 import com.yang.reggie.service.DishFlavorService;
 import com.yang.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +37,7 @@ public class DishController {
 
   @Autowired
   private CategoryService categoryService;
+  private int newState;
 
   /**
    * 新增菜品
@@ -133,6 +137,7 @@ public class DishController {
 
   /**
    * 根据条件查询对应的菜品数据
+   *
    * @param dish
    * @return
    */
@@ -181,6 +186,22 @@ public class DishController {
     }).collect(Collectors.toList());
 
     return R.success(dishDtos);
+  }
+
+  /**
+   * 起售停售状态切换
+   *
+   * @return
+   */
+  @PostMapping("/status/{status}")
+  public R<Boolean> toggleStatus(HttpServletRequest httpServletRequest, @PathVariable String status, @Param("ids") String ids) {
+    Dish dish = dishService.getById(ids);
+
+    dish.setStatus(Integer.valueOf(status));
+
+    dishService.updateById(dish);
+
+    return R.success(true);
   }
 }
 
